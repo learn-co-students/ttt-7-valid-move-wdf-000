c256df688c56d040cc728c6533a4b58089068e72
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-06-20 16:59:13 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  if position_taken?(board, index) || !(on_board?(index))
+    return false
+  else return true
+  end
+end
+
+def on_board?(index)
+  if index.between?(0, 8)
+    return true
+  end
+end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  empty_symbols = ["", " ", nil]
+  if empty_symbols.include?(board[position])
+    return false
+  end 
+return true
+end
