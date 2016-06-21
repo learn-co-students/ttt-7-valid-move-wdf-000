d58b457603d2e6a16c992f7c997d81f7e61ab62d
Fidel Severino <fidelscodes@gmail.com>
2016-06-21 16:53:44 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  if board.all? { |e| e == " " } && index.between?(0,8)
+    true
+  elsif position_taken?(board, index) == false && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# This is what we call a helper method
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  end
+end
