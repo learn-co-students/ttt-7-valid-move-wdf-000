4ebc3477d022ee64fbb1d86c4cd1942a3f5945b8
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-07-06 01:46:52 +0000

    Modified

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, index)
+  if(array[index] == " " || array[index] == "" || array[index] == nil)
+    false
+  else
+    true
+  end
+end
